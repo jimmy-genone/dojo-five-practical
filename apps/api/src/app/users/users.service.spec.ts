3cc9c6056a329f47a614a97a748a635f29c99c62
jimmy-genone <jimmy@genone.tech>
2022-02-11 10:21:54 -0500

    user service unit test

--- apps/api/src/app/users/users.service.spec.ts
+++ apps/api/src/app/users/users.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { User, UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -14,5 +14,19 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return null if user not found', async () => {
+    const user = await service.findOne("notauser");
+    expect(user).toBeUndefined()
+  });
+
+  it('should return user if found', async () => {
+    const user = await service.findOne("john");
+    expect(user).toMatchObject<User>({
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    })
   });
 });
